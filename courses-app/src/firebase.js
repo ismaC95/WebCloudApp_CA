c2bba2e8ce130bd6ea97c15668e34483ca6f9183
Julie <leehj2820@gmail.com>
2025-05-25 01:33:15 +0100

    dploy testing

--- courses-app/src/firebase.js
+++ courses-app/src/firebase.js
@@ -6,14 +6,24 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCyXb037zAt2a2YMUiyj38g0NZdqthETF0",
+//   authDomain: "coursesmarketplace.firebaseapp.com",
+//   projectId: "coursesmarketplace",
+//   storageBucket: "coursesmarketplace.firebasestorage.app",
+//   messagingSenderId: "955319533092",
+//   appId: "1:955319533092:web:e21fb22250917bf21c62b1",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCyXb037zAt2a2YMUiyj38g0NZdqthETF0",
-  authDomain: "coursesmarketplace.firebaseapp.com",
-  projectId: "coursesmarketplace",
-  storageBucket: "coursesmarketplace.firebasestorage.app",
-  messagingSenderId: "955319533092",
-  appId: "1:955319533092:web:e21fb22250917bf21c62b1",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
